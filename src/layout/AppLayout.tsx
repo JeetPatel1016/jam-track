fd3f1008fe50ffee51cf2209c65b1cae5878319c
Jeet Patel <Jeetpatel1011@gmail.com>
2025-07-23 09:07:56 -0400

    Added footer for sidebar switching

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,11 +1,18 @@
-import { Separator } from "@radix-ui/themes";
+import { Flex, Separator } from "@radix-ui/themes";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function AppLayout() {
   return (
-    <div style={{ width: "100vw" }}>
-      <Header />
-      <Separator size={"4"} />
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Flex direction={"column"} height={"100%"}>
+        <Header />
+        <Separator size={"4"} />
+        <div>Working Area</div>
+        <Separator size={"4"} mt={"auto"} />
+        <Footer />
+
+      </Flex>
     </div>
   );
 }
---
+++ src/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import { Flex, SegmentedControl } from "@radix-ui/themes";
+
+export default function Footer() {
+  return (
+    <Flex px="4" py="2">
+      <SegmentedControl.Root defaultValue="sections">
+        <SegmentedControl.Item value="sections">Sections</SegmentedControl.Item>
+        <SegmentedControl.Item value="tracks">Tracks</SegmentedControl.Item>
+      </SegmentedControl.Root>
+    </Flex>
+  );
+}
